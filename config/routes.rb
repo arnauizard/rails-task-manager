11652586eeee6c5e41cac6e1d21cbe548522f7e1
Arnau Izard Torredemer <arnau.izardtorredemer@solvaypostgrad.net>
2018-06-12 15:47:30 +0200

    rails with ActiveRecord

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # read all
+  get 'tasks', to: 'tasks#index'
+  # read one
+
+  # create new one
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post information to
+  post 'tasks', to: 'tasks#create'
+
+  # #update(get page with form)
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # #post information to with patch(updating)
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
+
 end
